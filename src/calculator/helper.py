94c0b56630831f2bb6a1b788221c44d34d6d14fd
tuannguyen_ <44093611+SledgyNguyen@users.noreply.github.com>
2022-08-04 00:21:26 +0700

    subtract function "sub()" added.

--- src/calculator/helper.py
+++ src/calculator/helper.py
@@ -9,3 +9,13 @@ def add(first_number, second_number):
     """
 
     return first_number + second_number
+
+
+def sub(first_number, second_number):
+    """
+    :param first_number:
+    :param second_number:
+    :return: difference of the between the first and second number
+    """
+
+    return first_number - second_number
