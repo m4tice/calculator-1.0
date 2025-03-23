b872a3d5ce37e5a51482119fdf0a03ddb818e6d4
m4tice <ductuan2908@gmail.com>
2025-03-23 08:51:13 +0700

    update: pylint fulfillment

--- setup.py
+++ setup.py
@@ -1,3 +1,17 @@
+"""
+Setup script for the py-dummy-app package.
+
+This script uses setuptools to package the application and define its entry points.
+The application can be run from the command line using the 'calculator' command.
+
+Attributes:
+    name (str): The name of the package.
+    version (str): The version of the package.
+    packages (list): A list of all Python import packages
+    that should be included in the distribution package.
+    entry_points (dict): A dictionary specifying
+    what scripts should be made available to the user's PATH.
+"""
 from setuptools import setup, find_packages
 
 setup(
